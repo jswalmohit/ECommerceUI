039f82004de8b0acbd26e14fae0dc3ab391c2146
Mohit <mohitjaiswal.bltr@gmail.com>
2025-11-22 23:14:02 +0530

    cleanup

--- src/app/features/cart/cart.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { CartPageComponent } from './pages/cart-page/cart-page.component';
-
-@NgModule({
-  imports: [CommonModule, CartPageComponent, RouterModule.forChild([{ path: '', component: CartPageComponent }])]
-})
-export class CartModule {}
