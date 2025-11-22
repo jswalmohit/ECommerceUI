24e9185d5ce96abafe7f147653f86fda40a8c7f8
mohit <43749932+jswalmohit@users.noreply.github.com>
2025-11-22 23:15:46 +0530

    cleanup (#1)
    * remove ssr
    
    * making standalone
    
    * making lazyloading
    
    * cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', loadComponent: () => import('./features/products/pages/product-list/product-list.component').then(m => m.ProductListComponent) },
+  { path: 'products', loadComponent: () => import('./features/products/pages/product-list/product-list.component').then(m => m.ProductListComponent) },
+  { path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
+  { path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent) },
+  { path: 'cart', loadComponent: () => import('./features/cart/pages/cart-page/cart-page.component').then(m => m.CartPageComponent) },
+  { path: '**', redirectTo: '' }
+];
