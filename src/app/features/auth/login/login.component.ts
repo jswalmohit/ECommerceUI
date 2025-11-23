6b976ef8c68ec7df4efe7029dd9b73cf19aec425
Mohit <mohitjaiswal.bltr@gmail.com>
2025-11-23 12:54:27 +0530

    login binding

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,24 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  model = { email: '', password: '' };
 
-  constructor(private auth: AuthService, private router: Router) {}
+  form;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', Validators.required]
+    });
+  }
 
   submit() {
-    if (this.auth.login(this.model.email, this.model.password)) {
-      this.router.navigate(['/']);
+    if (this.form.invalid) return;
+
+    const { email, password } = this.form.value;
+
+    if (this.auth.login(email!, password!)) {
+      this.auth.login(email!, password!).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
     }
   }
 }
