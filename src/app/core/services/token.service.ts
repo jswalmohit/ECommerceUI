4fd115b3c2e5696ff6adb79f06b54916c60c9b47
Mohit <mohitjaiswal.bltr@gmail.com>
2025-11-30 20:18:01 +0530

    cart

--- src/app/core/services/token.service.ts
+++ src/app/core/services/token.service.ts
@@ -4,6 +4,35 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenService {
+  private readonly TOKEN_KEY = 'user_token';
 
   constructor() { }
+
+  /**
+   * Get the stored JWT token
+   */
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  /**
+   * Set the JWT token
+   */
+  setToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  /**
+   * Remove the JWT token
+   */
+  removeToken(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  /**
+   * Check if token exists
+   */
+  hasToken(): boolean {
+    return !!localStorage.getItem(this.TOKEN_KEY);
+  }
 }
--- src/app/features/cart/services/cart.service.ts
+++ src/app/features/cart/services/cart.service.ts
@@ -2,22 +2,65 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { TokenService } from '../../../core/services/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
 
+  /**
+   * Add a product to cart
+   * JWT token will be automatically added by authInterceptor
+   */
   addToCart(productId: string): Observable<any> {
     const url = `${environment.apiBaseUrl}/api/Cart/add`;
     const headers = new HttpHeaders({
-      'CorrelationId': this.generateCorrelationId(),
-      'AuthToken': `Bearer ${localStorage.getItem('user_token') || ''}`
+      'CorrelationId': this.generateCorrelationId()
     });
 
     return this.http.post<any>(url, { productId }, { headers });
+  }
+
+  /**
+   * Get all cart items
+   * JWT token will be automatically added by authInterceptor
+   */
+  getCartItems(): Observable<any> {
+    const url = `${environment.apiBaseUrl}/api/Cart/items`;
+    const headers = new HttpHeaders({
+      'CorrelationId': this.generateCorrelationId()
+    });
+
+    return this.http.get<any>(url, { headers });
+  }
+
+  /**
+   * Remove item from cart
+   * JWT token will be automatically added by authInterceptor
+   */
+  removeFromCart(productId: string): Observable<any> {
+    const url = `${environment.apiBaseUrl}/api/Cart/remove/${productId}`;
+    const headers = new HttpHeaders({
+      'CorrelationId': this.generateCorrelationId()
+    });
+
+    return this.http.delete<any>(url, { headers });
+  }
+
+  /**
+   * Clear cart
+   * JWT token will be automatically added by authInterceptor
+   */
+  clearCart(): Observable<any> {
+    const url = `${environment.apiBaseUrl}/api/Cart/clear`;
+    const headers = new HttpHeaders({
+      'CorrelationId': this.generateCorrelationId()
+    });
+
+    return this.http.post<any>(url, {}, { headers });
   }
 
   private generateCorrelationId(): string {
