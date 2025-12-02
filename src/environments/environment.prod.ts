b692f222657275d91cd12d25bf5ec85a001aac82
Mohit <mohitjaiswal.bltr@gmail.com>
2025-12-02 23:28:31 +0530

    chi1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://ecommerceapp-m981.onrender.com',
-  imageBaseUrl: 'https://ik.imagekit.io/rkmart/RKMART/PRODUCTS'
+  imageBaseUrl: 'https://ik.imagekit.io/rkmart/RKMART/PRODUCTS',
+  tokenExpirationMinutes: 60
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'https://ecommerceapp-m981.onrender.com',
-  apiBaseUrl: 'https://localhost:7257',
-  imageBaseUrl: 'https://ik.imagekit.io/rkmart/RKMART/PRODUCTS'
+  apiBaseUrl: 'https://ecommerceapp-m981.onrender.com',
+  // apiBaseUrl: 'https://localhost:7257',
+  imageBaseUrl: 'https://ik.imagekit.io/rkmart/RKMART/PRODUCTS',
+  tokenExpirationMinutes: 5
 };
